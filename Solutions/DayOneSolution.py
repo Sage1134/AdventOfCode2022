0f181cc508b275e8b2aa861c98bf166c0deddfec
Sageman1134 <Raymondzheng2000@gmail.com>
2022-12-01 19:46:42 -0500

    Day One

---
+++ Solutions/DayOneSolution.py
@@ -0,0 +1,14 @@
+input = open("File.txt")
+lines = input.readlines()
+
+Value = 0
+Calories = []
+
+for i in lines:
+	if i != "\n":
+		Value += int(i)
+	else:
+		Calories.append(Value)
+		Value = 0
+
+print(Calories.pop(Calories.index(max(Calories))) + Calories.pop(Calories.index(max(Calories))) + Calories.pop(Calories.index(max(Calories))))
